371296b2b9bb3c23b5ab0dd42f618aab2e6b107c
Andrew Jones <acjones617@gmail.com>
2014-05-19 14:39:44 -0700

    Pulls in all dreams with different username and loads them

---
+++ client/scripts/inspirations.js
@@ -0,0 +1,23 @@
+var mydreams = angular.module('myApp.inspirations', [
+  'ui.router',
+  'ngAnimate',
+  'fx.animations',
+  'firebase']);
+
+app.controller('InspireC', function($scope, $state, $rootScope, $firebase) {  
+  $scope.username = $rootScope.user || 'test2';
+  $scope.popularDreams = [];
+  var refDreams = new Firebase("https://blazing-fire-3752.firebaseIO.com/dreams");
+  refDreams.on('value', function(snapshot) {
+    var allDreams = snapshot.val();
+    console.log(allDreams);
+    _.each(snapshot.val(), function(dream) {
+      if (dream.username !== $scope.username) {
+        $scope.popularDreams.push(dream);
+      }
+    })
+  })
+  
+  
+})
+
