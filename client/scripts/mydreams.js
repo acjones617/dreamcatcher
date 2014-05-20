e909c01bf6c12c79f608c939893b28db752595b1
Andrew Jones <acjones617@gmail.com>
2014-05-20 00:54:05 -0700

    Updates mydreams page logic

--- client/scripts/mydreams.js
+++ client/scripts/mydreams.js
@@ -5,12 +5,9 @@ var mydreams = angular.module('myApp.mydreams', [
   'firebase']);
 
 app.controller('DreamsC', function($scope, $state, $rootScope, $firebase) {
-  // $rootScope.on('login', function() {
-  //   $scope.user = user;
-  // })
-  
   $scope.username = $rootScope.user.username || 'test';
-  $scope.personalDreams = [];
+  $scope.personalDreams = $scope.personalDreams || [];
+
   var refDreams = new Firebase("https://blazing-fire-3752.firebaseIO.com/personal/"+$scope.username);
   refDreams.on('value', function(snapshot) {
     var allDreams = snapshot.val();
